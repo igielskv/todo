2987a4b2c0afa356f8a912003c1575cbb4653158
Manoli <igielskv@icloud.com>
2020-04-19 00:12:13 +0200

    Data we have got from server now displays in TableView.

--- todo/Controllers/TodoViewController.swift
+++ todo/Controllers/TodoViewController.swift
@@ -8,21 +8,45 @@
 
 import UIKit
 
-class TodoViewController: UIViewController {
-
+class TodoViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
     @IBOutlet weak var todoItemTextField: UITextField!
     @IBOutlet weak var prioritySegmentedControl: UISegmentedControl!
+    @IBOutlet weak var todoTable: UITableView!
+    
+    var todos = Array<Todo>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        NetworkService.shared.getTodos { (todos) in
-            print(todos)
-        }
+        
+        todoTable.delegate = self
+        todoTable.dataSource = self
+        
+        getTodos()
     }
 
+    func getTodos() {
+        NetworkService.shared.getTodos { (todos) in
+            self.todos = todos.items
+            self.todoTable.reloadData()
+        }
+    }
     @IBAction func addTodoButtonTapped(_ sender: Any) {
+        
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        todos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell") as? TodoTableViewCell {
+            cell.updateCell(todo: todos[indexPath.row])
+            return cell
+        }
+        
+        return UITableViewCell()
+    }
 }
 
--- todo/Views/TodoTableViewCell.swift
+++ todo/Views/TodoTableViewCell.swift
@@ -13,6 +13,21 @@ class TodoTableViewCell: UITableViewCell {
     @IBOutlet weak var todoItemLabel: UILabel!
     @IBOutlet weak var priorityView: UIView!
     
+    func updateCell(todo: Todo) {
+        todoItemLabel.text = todo.item
+        
+        switch todo.priority {
+        case 0:
+            priorityView.backgroundColor = #colorLiteral(red: 1, green: 0.8, blue: 0, alpha: 1)
+        case 1:
+            priorityView.backgroundColor = #colorLiteral(red: 1, green: 0.5843137255, blue: 0, alpha: 1)
+        case 2:
+            priorityView.backgroundColor = #colorLiteral(red: 1, green: 0.231372549, blue: 0.1882352941, alpha: 1)
+        default:
+            priorityView.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
